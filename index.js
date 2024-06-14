0df3aa862b8f21db11d1b6e245bb90866bec9fb3
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-06-14 12:52:40 +0200

    New route for get request

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res)=>{
    res.send("Hello World");
 })
 
+app.get('/api/courses', (req, res)=>{
+    res.send([1,2,3,4,5])
+})
+
 app.listen(3000, ()=> console.log("Server is listening on port 3000!"))
