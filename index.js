c0c38d633abd2db558b323dc0398e0da4571b1aa
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-06-15 21:03:28 +0200

    Handling HTTP GET Requests

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const app = express();
+const courses = [
+    {id: 1, name: "HTML"},
+    {id: 2, name: "CSS"},
+    {id: 3, name: "Javascript"}
+]
 
 app.get('/', (req, res)=>{
    res.send("Hello World");
 })
 
 app.get('/api/courses', (req, res)=>{
-    res.send([1,2,3,4,5])
+    res.send(courses)
 })
 
 app.get('/api/courses/:id', (req, res)=>{
-    res.send(req.params.id);
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send("Course not found")
+    res.send(course)
 })
 
 app.get('/api/posts/:year/:month', (req, res)=>{
     res.send(req.query)
 })
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Server is listening on port ${port}!`))
